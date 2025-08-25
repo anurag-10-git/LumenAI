a86d8c11bae727ebfbf720cb343c5a2b1925a83f
anurag-10-git <anuragmohan2022@gmail.com>
2025-08-25 21:20:29 +0530

    main UI completed

---
+++ src/config/gemini.jsx
@@ -0,0 +1,16 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: import.meta.env.VITE_API_KEY,
+});
+
+async function main(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+
+  return response.text
+}
+
+export default main;
