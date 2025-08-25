1a989a96842fc71d160cc0d78447c12856cc8703
anurag-10-git <anuragmohan2022@gmail.com>
2025-08-25 21:23:41 +0530

    small changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+import { useContext } from 'react'
 
 
 const Main = () => {
+  const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context)
+
   return (
     <div className='main'>
       <div className="nav">
@@ -10,6 +14,7 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
+        {!showResult ? <>
         <div className="greet">
           <p><span>Hello, Dev.</span></p>
           <p>How can i help you today?</p>
@@ -32,13 +37,14 @@ const Main = () => {
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+        </> : <div className='result'></div> }
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder='Enter a prompt here!' />
+            <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder='Enter a prompt here!' />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img src={assets.send_icon} onClick={() => onSent()} alt="" />
             </div>
           </div>
           <p className='bottom-info'>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,22 +1,46 @@
-import { createContext } from "react";
+import { createContext, useCallback, useState } from "react";
 import main from "../config/gemini";
 
-const Context = createContext()
+export const Context = createContext()
 
-const onSent = async (prompt) => {
-  await main(prompt);
-}
+
 
 const ContextProvider = ({ children }) => {
+  const [input, setInput] = useState("")
+  const [recentPrompt, setRecentPrompt] = useState("")
+  const [prevPrompts, setPrevPrompts] = useState([])
+  const [showResult, setShowResult] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [resultData, setResultData] = useState("")
 
-  onSent("What is React?");
+  const onSent = useCallback(async () => {
+    setResultData("")
+    setLoading(true)
+    setShowResult(true)
+    const response = await main(input)
+    console.log(response)
+    setResultData(response)
+    setLoading(false)
+    setInput("")
+  }, [input])
 
   const contextValue = {
-
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+    // setResultData,
+    // setShowResult,
   }
 
   return (
-    <Context.Provider value={{}}>
+    <Context.Provider value={contextValue}>
       {children}
     </Context.Provider>
   )
